7f605337c2465facd893cfa6636845b0f3babfce
Bipin Singh <bipinsingh8052@gmail.com>
2024-12-05 12:34:40 +0530

    img

--- first_program/src/App.jsx
+++ first_program/src/App.jsx
@@ -3,25 +3,30 @@ import Navbar from "./Navbar"
 // import  './App.css'
 
 
+
 export default function App() {
 
   // internal css in react  but as a object form
-  let st={
-    backgroundColor:"blue",
-    color:"orange"
+  // let st={
+  //   backgroundColor:"blue",
+  //   color:"orange"
 
-  }
+  // }
   return (
-    <>
-      {/* inline css */}
-      <h1 style={{backgroundColor:"red",color:"white"}}>
+    // <>
+    //   {/* inline css */}
+    //   <h1 style={{backgroundColor:"red",color:"white"}}>
 
-        hello
-      </h1>
-      <h2 style={st}>hello world</h2>
-      <pre className="header">jskaanjkaeno</pre>
-      <Navbar/>
-    </>
+    //     hello
+    //   </h1>
+    //   <h2 style={st}>hello world</h2>
+    //   <pre className="header">jskaanjkaeno</pre>
+    //   <Navbar/>
+    // </>
+    <Navbar/>
+
+
+
   )
 }
 
--- first_program/src/Navbar.jsx
+++ first_program/src/Navbar.jsx
@@ -1,6 +1,8 @@
+import './Navbar.css'
 
 function Navbar() {
   return (
+    <>
     <div>
       <nav>
         <h1>logo</h1>
@@ -12,6 +14,12 @@ function Navbar() {
         </ul>
       </nav>
     </div>
+    <div className='hero'>
+      <marquee behavior="auto" direction="left"><h1>My world</h1></marquee>
+      <marquee behavior="auto" direction="right-left"><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi consequatur neque debitis a saepe rerum, reprehenderit hic obcaecati repudiandae, officia cupiditate at, nihil doloribus deserunt adipisci corporis asperiores animi distinctio.</p></marquee>
+    </div>
+    <img src="img.jpg" alt="" />
+    </>
   )
 }
 
