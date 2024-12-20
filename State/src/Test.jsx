c28067bb046257f0a5b0f37953b6ec904b9df4ea
Bipin Singh <bipinsingh8052@gmail.com>
2024-12-20 15:46:53 +0530

    task is completed and also learn it how to apply it use state

--- State/src/Test.jsx
+++ State/src/Test.jsx
@@ -3,10 +3,10 @@ import { useState } from "react"
 
 function Test() {
     let [counter,setcounter]=useState(0);
-    let [bg,setbg]=useState("white");
+    let [bg,setbg]=useState("black");
     function show(){
-        
         setcounter(counter+1);
+        // console.log(bg);
     }
   return (
     <>
@@ -14,7 +14,8 @@ function Test() {
       <h1>{counter}</h1>
     <button className="button" onClick={show}>count</button>
     </div>
-    <div className="image" style={{backgroundColor:{bg},width:"100vh",height:"100vh",border:"1px solid black"}}>
+
+    <div className="image" style={{backgroundColor:bg,width:"100vh",height:"100vh",border:"1px solid black"}}>
 
         <button onClick={()=>setbg("blue")}>Blue</button>
         <button onClick={()=>setbg("red")}>red</button>
