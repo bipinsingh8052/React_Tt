c2251d5dcb0e788c514575742e923b48d9f45dcb
Bipin Singh <bipinsingh8052@gmail.com>
2024-12-23 16:51:11 +0530

    use effect hook is working

--- Effect_useEffect/src/Effect.jsx
+++ Effect_useEffect/src/Effect.jsx
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react"
 
 
 function Effect() {
-    const{bg,setbg}=useState("black")
+    const[bg,setbg]=useState("black")
     useEffect(()=>{alert("Change the bg color!!!")},[bg]);
   return (
     <div>
-        <div style={{width:"100vh",height:"100vh" ,border:"2px solid black",backgroundColor:{bg}}}>
-            <button onClick={()=>setbg("red")}>Red</button>
+        <div style={{width:"100vh",height:"100vh" ,border:"2px solid black",backgroundColor:bg}}>
+            <button onClick={()=>setbg("white")}>Red</button>
             <button onClick={()=>setbg("green")}>green</button>
             <button onClick={()=>setbg("orange")}>orange</button>
             <button onClick={()=>setbg("black")}>black</button>
