a1f91fddc337f53094e7ef4cae06c8350915159d
Bipin Singh <bipinsingh8052@gmail.com>
2024-12-27 16:55:25 +0530

    Key

--- Form_Validation/src/Map_method.jsx
+++ Form_Validation/src/Map_method.jsx
@@ -1,3 +1,6 @@
+// Key is given the unique in the array and by the help of key you can updated and delete the element 
+
+
 
 function Map_method() {
     let ar=["bipins ",9088,"kumer","singh","ram"]
@@ -33,11 +36,20 @@ function Map_method() {
             year:1009
         }
     ]
+
+
     let arr_data =arr_of_object.map((e,index)=>{return <li key={index}><p>Name{e.name}</p>Age{e.age}Year{e.year}</li>});
     let data =ar.map((e,index)=>{return <li key={index}>{e}</li>})
+
+
+
+    let find= arr_data.map((e)=>{return e.key==1})
+    console.log(find);
     console.log(data);
   return (
     <>
+
+    {/* you can also do it direced */}
     {ar.map((e,index)=>{return <h1 key={index}>{e}</h1>})}
     <ul>
         {data}
