24fabd029410c05c7d91004cfd0dc46de401f75d
Bipin Singh <bipinsingh8052@gmail.com>
2025-01-10 12:24:34 +0530

    add data in json

--- Axios/src/Fetch_data.jsx
+++ Axios/src/Fetch_data.jsx
@@ -2,11 +2,36 @@ import { useEffect, useState } from "react"
 import axios from 'axios'
 export default function Fetch_data() {
     let [datainfo,setdatainfo]=useState([]);
+    let [frmin,setfrmin]=useState({
+      name: "",
+      age: "",
+      contact:"" ,
+      city: ""
+    })
+      // Delete data by the help of id
     function myDel(id){
         axios.delete(`http://localhost:3000/all_data/${id}`)
-        .then(re=>alert("your data is deleted"));
+        .then(re=>alert("your data is deleted"))
+    }
+
+
+
+    function inputfrom(e){
+      let {name,value}=e.target;
+      setfrmin({
+        ...frmin,
+        [name]:value
+      })
+    }
+
+    // add the data in from that json
+    function submitfrom(e){
+      e.preventDefault();
+      axios.post("http://localhost:3000/all_data",frmin)
+      .then(res=>alert("Insert the data okkk"));
     }
     useEffect(()=>{
+      // fetch data in the api
         axios.get("http://localhost:3000/all_data")
         .then(res=>{
             console.log(res)
@@ -14,6 +39,10 @@ export default function Fetch_data() {
 
         })
     },[myDel])
+
+
+
+ 
     
   return (
     <>
@@ -41,6 +70,7 @@ export default function Fetch_data() {
                 }
                   ) */}
                 {/* } */}
+                {/*Add the data by the help of jsx  */}
                 {
                     datainfo.map((e,index)=>{
                         return( 
@@ -59,6 +89,25 @@ export default function Fetch_data() {
             </tbody>
         </table>
       </div>
+
+<br /><br /><br />
+
+      <form onSubmit={submitfrom}>
+        <label htmlFor="">Name</label>
+        <input type="text" placeholder="Enter the Name:" value={frmin.name} name="name" onChange={inputfrom} />
+        <br /><br />
+        <label htmlFor="">Age</label>
+        <input type="text" placeholder="Enter the Age:"value={frmin.age} name="age" onChange={inputfrom} />
+        <br /><br />
+        <label htmlFor="">contact</label>
+        <input type="text" placeholder="Enter the contact:" value={frmin.contact} name="contact" onChange={inputfrom} />
+        <br /><br />
+
+        <label htmlFor="">City</label>
+        <input type="text" placeholder="Enter the City:" value={frmin.city} name="city" onChange={inputfrom} />
+        <br /><br />
+        <button type="submit">submit </button>
+      </form>
     </>
   )
 }
