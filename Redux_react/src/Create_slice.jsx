c14fb28b1fa79ad2277e40cf4a9eed5efdd7a306
Bipin Singh <bipinsingh8052@gmail.com>
2025-01-20 17:17:16 +0530

    redux start first commend react-redux and reduxjs/toolkit

---
+++ Redux_react/src/Create_slice.jsx
@@ -0,0 +1,15 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+
+const counter =createSlice({
+    name:"counter",
+    initialState:{value:0},
+    reducers:{
+        inc:(state)=>{state.value+=1},
+        dec:(state)=>{state.value-=1}
+    }
+})
+
+// decher the object 
+export  const {inc,dec}=counter.actions;
+export default counter.reducer;
